e1a58807d14120be05fd841b7c74340ad6a4d325
Durahman Firmansyah Alamsyah <durahman@jkt1.ebdesk.com>
2022-05-20 10:28:33 +0700

    add sentry

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+
+Sentry.init({
+  Vue,
+  dsn: "https://19d510a6eb604b8ba56457eaac88b844@o1253343.ingest.sentry.io/6420257",
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["localhost", "my-site-url.com", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 new Vue({
   router,
   store,
   vuetify,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <h1>Welcome to Your Vue.js App</h1>
+
+    <v-btn color="primary" @click="throwError">Throw Error</v-btn>
   </div>
 </template>
 
@@ -8,5 +10,10 @@
 export default {
   name: "Home",
   components: {},
+  methods: {
+    throwError() {
+      throw Error("Ini Error dur");
+    },
+  },
 };
 </script>
